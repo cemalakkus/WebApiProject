084094bcd12d41df33e32abc8b10aed237275e39
cemalakkus <devcakkus@outlook.com>
2024-08-07 22:49:51 +0300

    appsettings ve launchsettings konfigurasyonları yapıldı.

--- Presentation/WebApiProject.Api/Program.cs
+++ Presentation/WebApiProject.Api/Program.cs
@@ -6,6 +6,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var env = builder.Environment;
+
+//Prod ve Dev ortamýn config ayarlarýnýn otomatikleþtirme iþlemleri.
+builder.Configuration
+    .SetBasePath(env.ContentRootPath) //WebApiProject.Api nin bulunduðu path'i local veya sunucu olsun otomatik olarak alýr.
+    .AddJsonFile("appsettings.json", optional: false) //Her zaman bu dosyayý okuyacak.
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true); //Burasý olabilir veya olmayabilir. Opsiyoneldir.
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
