b750ca10062b5d732144e204d0e5c82fc8da8b2b
cemalakkus <devcakkus@outlook.com>
2024-08-07 23:26:21 +0300

    entityler oluşturuldu.

---
+++ Core/WebApiProject.Domain/Common/IEntityBase.cs
@@ -0,0 +1,5 @@
+﻿namespace WebApiProject.Domain.Common;
+
+public interface IEntityBase
+{
+}
---
+++ Core/WebApiProject.Domain/Entities/Detail.cs
@@ -0,0 +1,21 @@
+﻿using WebApiProject.Domain.Common;
+
+namespace WebApiProject.Domain.Entities;
+
+public class Detail : EntityBase
+{
+    public Detail()
+    {
+
+    }
+    public Detail(string title, string description, int categoryId)
+    {
+        Title = title;
+        Description = description;
+        CategoryId = categoryId;
+    }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public required int CategoryId { get; set; }
+    public Category Category { get; set; }
+}
